eb6f49d4b4fdd83b52657a5685ea3c5fc7c36828
Maria Planella Vall <mplanellav@uoc.edu>
2025-11-29 22:07:07 +0100

    Primer commit

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,15 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+
+// import 'some-node-module';
+// import SomeModule from 'some-node-module';
+
+/**
+ * Write any other JavaScript below
+ */
+
+// assets/scripts/main.js
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
