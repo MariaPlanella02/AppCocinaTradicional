13c0c6269691210e5db3299e8e39212c7a8799fa
Maria Planella Vall <mplanellav@uoc.edu>
2025-12-04 22:30:25 +0100

    Update_info_pagines

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -10,6 +10,16 @@
  * Write any other JavaScript below
  */
 
-// assets/scripts/main.js
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
+//Activar menú en dispositivo pequeño
+document.addEventListener("DOMContentLoaded", () => {
+  const toggle = document.querySelector(".header__toggle");
+  const nav = document.querySelector(".header__nav");
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("is-active");
+    });
+  }
+});
